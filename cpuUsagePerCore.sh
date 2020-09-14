d76499bad25684b562ebd9aac41bd5c25cc07c96
wmsegar <wayne.segar@gmail.com>
2020-09-14 19:10:06 +0000

    updated output command

--- cpuUsagePerCore.sh
+++ cpuUsagePerCore.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-	mpstat -P ALL 1 1 | tail -4 | awk {' gsub(",", ".", $3); gsub(",", ".", $12); print "host.cpu.usr,core=core"$2" "$3 "\nhost.cpu.idle,core=core"$2" "$12'} | /opt/dynatrace/oneagent/agent/tools/dynatrace_ingest -v
+	mpstat -P ALL 1 1 | tail -3 | awk {' gsub(",", ".", $3); gsub(",", ".", $12); print "host.cpu.usr,core=core"$2" "$3 "\nhost.cpu.idle,core=core"$2" "$12'} | /opt/dynatrace/oneagent/agent/tools/dynatrace_ingest -v
 	sleep 10
 done
